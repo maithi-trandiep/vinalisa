768096ff9f536e12ab18aaf911c313e8a8343c43
thi-tdm <thi.tdm04@gmail.com>
2020-05-19 21:07:59 +0200

    Edit some css

--- src/Components/GalleryVin.js
+++ src/Components/GalleryVin.js
@@ -8,11 +8,11 @@ export default function GalleryVin ({ id, title, subtitle }) {
                 <h3 className="section-subheading text-muted">{subtitle}</h3>
             </div>
             <div className="">
-                <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
+                <div id="carouselCaptions" className="carousel slide" data-ride="carousel">
                     <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
-                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                        <li data-target="#carouselCaptions" data-slide-to="0" className="active"></li>
+                        <li data-target="#carouselCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselCaptions" data-slide-to="2"></li>
                     </ol>
                     <div className="carousel-inner">
                         <div className="carousel-item active">
@@ -37,11 +37,11 @@ export default function GalleryVin ({ id, title, subtitle }) {
                         </div>
                         </div>
                     </div>
-                    <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <a className="carousel-control-prev" href="#carouselCaptions" role="button" data-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="sr-only">Previous</span>
                     </a>
-                    <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <a className="carousel-control-next" href="#carouselCaptions" role="button" data-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="sr-only">Next</span>
                     </a>
